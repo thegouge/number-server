8b5e40f44cbf247295b800b368b0bfc953c6e7fe
theGouge <thegouge42@gmail.com>
2018-04-01 17:58:21 -0700

    Created Static Version of App

---
+++ source/main.js
@@ -0,0 +1,12 @@
+
+var output = parseInt(document.getElementById('output').innerHTML);
+
+function decrease() {
+    output--;
+    document.getElementById('output').innerHTML = output;
+}
+
+function increase() {
+    output++;
+    document.getElementById('output').innerHTML = output;
+}
