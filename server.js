aeba8d78a4c66a7df4f406f72ef499f4ed8f176a
theGouge <thegouge42@gmail.com>
2018-04-08 19:22:39 -0700

    serves a static HTML page through the server

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 var counter = 0;
 
 var port = 3000;
 
+app.use('/assets', express.static(__dirname + '/assets'));
+
 app.get('/', (req, res) => {
-    counter++;
-    res.send(`you are person number ${counter} visiting the webpage!`);
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(port, () => console.log('Server Running at localhost:' + port));
+app.listen(port, () => {
+    console.log('Server Running at localhost:' + port);
+});
+
+/* 
+    Response Status codes
+        100: ???
+        200: success
+        300: redirect
+        400: client side error
+        500: server error
+
+    unpkg = unpackage = host for different js stuffs
+*/
--- source/main.js
+++ source/main.js
@@ -1,11 +1,3 @@
-var output = parseInt(document.getElementById('output').innerHTML);
+function minus () {}
 
-function decrease() {
-    output--;
-    document.getElementById('output').innerHTML = output;
-}
-
-function increase() {
-    output++;
-    document.getElementById('output').innerHTML = output;
-}
+function plus () {}
